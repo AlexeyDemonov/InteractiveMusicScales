58ae9c96699a0762fc4bd8dda7858a3b656e3bdd
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-10-30 12:23:28 +0500

    NoteValue enum

---
+++ InteractiveMusicScales/Enums/NoteValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveMusicScales
+{
+    enum NoteValue
+    {
+        C   = 1,
+        Cd  = 2,
+        D   = 4,
+        Dd  = 8,
+        E   = 16,
+        F   = 32,
+        Fd  = 64,
+        G   = 128,
+        Gd  = 256,
+        A   = 512,
+        Ad  = 1024,
+        B   = 2048,
+    }
+}
