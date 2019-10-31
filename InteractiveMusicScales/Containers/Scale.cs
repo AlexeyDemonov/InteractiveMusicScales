3a295cba2f8ac5e96c9f30c4853752ef6b3cda88
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-10-31 12:07:43 +0500

    Added Scale container

---
+++ InteractiveMusicScales/Containers/Scale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveMusicScales
+{
+    class Scale
+    {
+        public string Name { get; }
+        public Sound Sound { get; }
+        public Sound Keynote { get; }
+
+        public Scale(string name, Sound sound, Sound keynote)
+        {
+            this.Name = name;
+            this.Sound = sound;
+            this.Keynote = keynote;
+        }
+    }
+}
