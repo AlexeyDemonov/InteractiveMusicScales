586ca169edac1c1a0128e8d3553da38a8371548a
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-11-04 16:51:24 +0500

    Fretboard layout changed Changed: Height parameter at Add/Remove string mechanism moved from converter class to XAML Changed: Removed ColumnDefinition and RowDefinition at Fretboard grid style as it is unnecessary anymore

--- InteractiveMusicScales/BoolToHeightConverter.cs
+++ InteractiveMusicScales/BoolToHeightConverter.cs
@@ -9,13 +9,24 @@ using System.Windows.Data;
 
 namespace InteractiveMusicScales.Interface
 {
+    /// <summary>
+    /// Converts boolean value to height
+    /// </summary>
     class BoolToHeightConverter : IValueConverter
     {
+        /// <summary>
+        /// Returns height specified by convertion parameter or zero based on incoming boolean value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if(value != null && value is bool)
+            if(value != null && parameter != null && value is bool && int.TryParse(parameter.ToString(), out int height) == true )
             {
-                return ((bool)value == true) ? 30 : 0;
+                return ((bool)value == true) ? height : 0;
             }
             else
                 return DependencyProperty.UnsetValue;
