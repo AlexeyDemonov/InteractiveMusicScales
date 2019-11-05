1f801d2702d21fcdc0cec6c05836848d4a617e63
alexei.demonov.341 <alexei.demonov.341@gmail.com>
2019-11-05 11:50:56 +0500

    Scale selection rework (unfinished) Added: ScalesToScaleButtons converter Changed: ListBox to ItemsControl with a stack panel of buttons Now scales are presented as buttons, that is done for the possibility of applying the scale by clicking on it (after playing with note buttons), previously to apply the scale you were have to choose another one and choose this once again Known issues: Scale button is not working yet - some troubles with command binding

---
+++ InteractiveMusicScales/InterfaceSubClasses/Converters/ScalesToScaleButtonsConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace InteractiveMusicScales.Interface
+{
+    class ScalesToScaleButtonsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null && value is Scale[])
+            {
+                var scales = (Scale[])value;
+                int length = scales.Length;
+
+                var buttons = new ScaleButton[ length ];
+
+
+                for (int i = 0; i < length; i++)
+                {
+                    buttons[i] = new ScaleButton() { Scale = scales[i] };
+                }
+
+                return buttons;
+            }
+            else
+                return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
